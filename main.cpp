ac26ca036312cb912b56cd0285cd78d3459a9eac
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-07 13:22:28 -0700

    Adds explanatory comments

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 #include <vector>
 using namespace std;
 
+const int WIDTH = 10;
 // providing a value for the random seed instead of generating it to ensure consistency while testing
 const int SEED = 3;
-const int WIDTH = 10;
 default_random_engine generator(SEED);
 
 struct Color {
-    int red;
-    int green;
-    int blue;
+	int red;
+	int green;
+	int blue;
 };
 
 // getRandomNum() generates a random number between the low and high integers
@@ -28,41 +28,44 @@ int getRandomNum(int low, int high);
 // returns: nothing
 void displayVectorAsTable(const vector<Color> &colorVect);
 
-int main(){
-    vector<Color> colors;
-    int n = getRandomNum(25, 50);
+int main() {
+	vector<Color> colors;
+	int n = getRandomNum(25, 50);
+	Color temp;
 
-    Color temp;
-    for (int i = 0; i < n; i++) {
-        temp.red = getRandomNum(0, 255);
-        temp.green = getRandomNum(0, 255);
-        temp.blue = getRandomNum(0, 255);
-        colors.push_back(temp);
-    }
+	for (int i = 0; i < n; i++) {
+		temp.red = getRandomNum(0, 255);
+		temp.green = getRandomNum(0, 255);
+		temp.blue = getRandomNum(0, 255);
+		colors.push_back(temp);
+	}
 
-    displayVectorAsTable(colors);
+	displayVectorAsTable(colors);
 
-    return 0;
+	return 0;
 }
 
 int getRandomNum(int low, int high) {
-    uniform_int_distribution<> distribution(low, high);
-    return distribution(generator);
+	// This will produce random integers in an inclusive range from low to high where each number is equally likely to be produced
+	uniform_int_distribution<int> distribution(low, high);
+	// Generates a new random number according to the distribution using the random generator seeded earlier
+	return distribution(generator);
 }
 
 void displayVectorAsTable(const vector<Color> &colorVect) {
-    // heading
-    cout << setw(WIDTH) << "Color#" << setw(WIDTH)<< "R value" << setw(WIDTH) << "G value" << setw(WIDTH)
-        << "B value" << endl;
-    cout << setw(WIDTH) << "______" << setw(WIDTH)<< "_______" << setw(WIDTH) << "_______" << setw(WIDTH)
-        << "_______" << endl;
+	// heading
+	cout << setw(WIDTH) << "Color#" << setw(WIDTH) << "R value" << setw(WIDTH) << "G value" << setw(WIDTH)
+			<< "B value" << endl;
+	cout << setw(WIDTH) << "______" << setw(WIDTH) << "_______" << setw(WIDTH) << "_______" << setw(WIDTH)
+			<< "_______" << endl;
 
-    int count = 1;
-    for (Color cur : colorVect) {
-        cout << setw(WIDTH) << count;
-        cout << setw(WIDTH) << cur.red;
-        cout << setw(WIDTH) << cur.green;
-        cout << setw(WIDTH) << cur.blue << endl;
-        count++;
-    }
+	// vector data
+	int count = 1;
+	for (Color cur : colorVect) {
+		cout << setw(WIDTH) << count;
+		cout << setw(WIDTH) << cur.red;
+		cout << setw(WIDTH) << cur.green;
+		cout << setw(WIDTH) << cur.blue << endl;
+		count++;
+	}
 }
