8bfb518ab27acc703a010ab7cb98e0ee94697880
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-06 22:14:14 -0700

    Adds getRandomNum() function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 // COMSC-210 | Lab 4a | Dainiz Almazan
 // IDE used: CLion
 #include <iostream>
+#include <random>
 #include <vector>
 using namespace std;
+
+const int SEED = 3;
+default_random_engine generator(SEED);
 
 struct Color {
     int red;
@@ -10,8 +14,19 @@ struct Color {
     int blue;
 };
 
+// getRandomNum() generates a random number between the low and high integers
+// arguments: an integer for the start of the range and an integer for the end of the range
+// returns: a random int value between the low and high integers
+int getRandomNum(int low, int high);
+
 int main(){
     vector<Color> colors;
+    int n = getRandomNum(25, 50);
 
     return 0;
+}
+
+int getRandomNum(int low, int high) {
+    uniform_int_distribution<> distribution(low, high);
+    return distribution(generator);
 }
